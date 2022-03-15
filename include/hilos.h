bbc3bb722dcf913d8a90c38ec09432ee62dcabdc
Shenxiao Lin <shenxiao.lin@estudiante.uam.es>
2022-03-15 14:57:41 +0000

    hilos.h > comentarios pt1

--- include/hilos.h
+++ include/hilos.h
@@ -12,18 +12,41 @@ typedef void* (*funcionHilo)(void*);
 
 
 
-/*Funciones de gestion*/
+/*
+ * Funciones del gestor de hilos
+ */
 
-/*Create*/
+/*
+ * FUNCION: GestorHilos* hilo_getGestor(int maxHilos)
+ * ARGS_IN: int - numero max de hilos que tendra
+ * DESCRIPCION: Crea el gestor de hilos
+ * ARGS_OUT: GestorHilos* - devuelve un gestor de hilos si fue correctamente creado
+ *              y no habia otro creado previamente. Si ya habia uno creado, se retorna ese. 
+ *              Si se produce un error, retorna NULL
+ */
 GestorHilos* hilo_getGestor(int maxHilos);
 
-/*Close hilos*/
+/*
+ * FUNCION: int hilo_closeHilos(GestorHilos* gh)
+ * ARGS_IN: GestorHilos* - gestor de hilos
+ * DESCRIPCION: Cierra todos los hilos activos
+ * ARGS_OUT: int - devuelve 1 en caso de exito, 0 en caso de error
+ */
 int hilo_closeHilos(GestorHilos* gh);
 
-/*Destroy*/
+/*
+ * FUNCION: int hilo_closeHilos(GestorHilos* gh)
+ * ARGS_IN: GestorHilos* - gestor de hilos
+ * DESCRIPCION: Destruye el gestor de hilos, lberando la memoria de los hilos que controlaba este
+ * ARGS_OUT: int - devuelve 0
+ */
 int hilo_destroyGestor(GestorHilos* gh);
 
-/*Force destroy*/
+/*
+ * FUNCION: int hilo_closeHilos(GestorHilos* gh)
+ * ARGS_IN: GestorHilos* - gestor de hilos
+ * DESCRIPCION: Fuerza la detencion de todos los hilos activos, liberando incluso el gestor de hilos
+ */
 void hilo_forceDestroyGestor(GestorHilos* gh);
 
 
