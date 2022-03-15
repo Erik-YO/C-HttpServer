81ba3cbaf3357e0bcc1be8c15870d341bcd34d84
Erik Yuste Ortiz <erik.yuste@estudiante.uam.es>
2022-03-15 12:47:49 +0000

    Comentarios

--- include/process.h
+++ include/process.h
@@ -5,19 +5,29 @@
 
 #include "types.h"
 
-/* Codigos arbitrarios */
-#define METHOD_GET 9224
-#define METHOD_POST 9326
-#define METHOD_OPTIONS 9556
 
 #define GET "GET"
 #define POST "POST"
 #define OPTIONS "OPTIONS"
 
+/*
+ * FUNCION: void process_request(int connfd)
+ * ARGS_IN: int - descriptor del socket por el que mandar la respuesta
+ * DESCRIPCION: procesa las peticiones http que lleguen por el socket y las responden 
+ */
 void process_request(int connfd);
 
+/*
+ * FUNCION: void process_setSignalHandler()
+ * DESCRIPCION: establece el manejador de senales de la senal SIGINT
+ */
 void process_setSignalHandler();
 
+/*
+ * FUNCION: int process_endProcess()
+ * DESCRIPCION: comprueba si process ha recibido la senal SIGINT o no
+ * ARGS_OUT: int - resultado: 1 senal recibida, 0 senal no recibida
+ */
 int process_endProcess();
 
 #endif
